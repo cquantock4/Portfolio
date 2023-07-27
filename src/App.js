5a3160ca26c2521cdcc9837f818a4cb82c42e15e
Cody Quantock <codyquantock@gmail.com>
2023-07-27 14:51:27 -0400

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Overview from './pages/overview'
+import Projects from './pages/projects'
+
+//bootstrap
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Table from 'react-bootstrap/Table';
+import InputGroup from 'react-bootstrap/InputGroup';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Badge from 'react-bootstrap/Badge';
+import Modal from 'react-bootstrap/Modal';
+
+import './assets/styles/main.css'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Website Under Construction
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/cody-quantock-b86884b3/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View My LinkedIn
-        </a>
-      </header>
-    </div>
+    <Container fluid="xxxl" style={{padding: 15, height: '100vh'}}>
+      <Tab.Container defaultActiveKey="workouts">
+        <Nav variant="tabs">
+          <div style={{marginRight: 20, paddingLeft: 5}}>
+            <h4 className="title">Cody Quantock</h4>
+          </div>
+          <Nav.Item>
+            <Nav.Link eventKey="overview">Overview</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="projects">Projects</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content>
+          <Tab.Pane eventKey="overview">
+            <Container fluid className='p-3'>
+              <Overview />
+            </Container>
+          </Tab.Pane>
+          <Tab.Pane eventKey="projects">
+            <Container fluid className='p-3'>
+              <Projects />
+            </Container>
+          </Tab.Pane>
+        </Tab.Content>
+      
+      
+      </Tab.Container>
+    </Container>
   );
 }
 
---
+++ src/pages/projects/index.js
@@ -0,0 +1,36 @@
+import { useState, useEffect, useRef } from 'react';
+
+
+
+//bootstrap
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Table from 'react-bootstrap/Table';
+import InputGroup from 'react-bootstrap/InputGroup';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Badge from 'react-bootstrap/Badge';
+import Modal from 'react-bootstrap/Modal';
+
+
+
+function Projects() {
+
+    return (
+        <Container>
+            <Row>
+                <h2>Projects</h2>
+            </Row>
+        </Container>
+    )
+    
+  }
+  
+  export default Projects;
